cdffe1c500c3ec41753b84e6d6f9cc17a1182631
oriongeuse4678021-art <oriongeuse4678021@gmail.com>
2026-06-14 14:46:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,42 +3,41 @@ const WebSocket = require('ws');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('MC Portal Server Running!');
+  res.end('OK');
 });
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log('マイクラが接続しました！');
+  console.log('接続されました！');
 
   ws.on('message', (data) => {
     try {
-      const msg = JSON.parse(data);
+      const msg = JSON.parse(data.toString());
       console.log('受信:', JSON.stringify(msg));
 
-      ws.send(JSON.stringify({
-        body: { statusCode: 0 },
-        header: {
-          messagePurpose: 'commandResponse',
-          requestId: msg.header?.requestId || '00000000-0000-0000-0000-000000000000',
-          version: 1
-        }
-      }));
+      const purpose = msg.header?.messagePurpose;
+
+      if (purpose === 'subscribe') {
+        ws.send(JSON.stringify({
+          body: { statusCode: 0 },
+          header: {
+            messagePurpose: 'commandResponse',
+            requestId: msg.header?.requestId || '',
+            version: 1
+          }
+        }));
+      }
     } catch(e) {
       console.log('エラー:', e);
     }
   });
 
-  ws.on('close', () => {
-    console.log('切断されました');
-  });
-
-  ws.on('error', (err) => {
-    console.log('WSエラー:', err);
-  });
+  ws.on('close', () => console.log('切断'));
+  ws.on('error', (e) => console.log('エラー:', e));
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`サーバー起動中: ${PORT}`);
+  console.log('起動中:', PORT);
 });
